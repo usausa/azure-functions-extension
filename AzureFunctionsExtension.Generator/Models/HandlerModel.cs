b2cb6d343334654d680d3b939f214de9a1fe46a1
usausa <machi.pon@gmail.com>
2026-08-10 13:08:59 +0900

    Update

--- AzureFunctionsExtension.Generator/Models/HandlerModel.cs
+++ AzureFunctionsExtension.Generator/Models/HandlerModel.cs
@@ -16,15 +16,19 @@ internal enum ResponseType
 }
 
 internal sealed record HandlerModel(
+    // Method signature
     string MethodName,
     HandlerType Type,
     bool IsAsync,
     TypeRefModel? ResultType,
     ResponseType ResponseType,
     EquatableArray<ParameterModel> Parameters,
+    // Http trigger
     string? HttpMethod,
     string? Route,
     string? AuthorizationLevel,
+    // Timer trigger
     string? TimerSchedule,
+    // Queue trigger
     string? QueueName,
     string? QueueConnection);
--- AzureFunctionsExtension.Tests/CompilationHelper.cs
+++ AzureFunctionsExtension.Tests/CompilationHelper.cs
@@ -1,6 +1,8 @@
 namespace AzureFunctionsExtension.Tests;
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 
 using AzureFunctionsExtension.Annotations;
 using AzureFunctionsExtension.Generator;
@@ -9,82 +11,38 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using SourceGenerateHelper.Testing;
 internal static class CompilationHelper
 {
-    private const string GlobalUsings =
-        """
-        global using System;
-        global using System.Collections.Generic;
-        global using System.Linq;
-        global using System.Threading;
-        global using System.Threading.Tasks;
-        """;
+    private static GeneratorTestRunner Runner => GeneratorTestRunner
+        .For<FunctionGenerator>()
+        .WithReference(typeof(AzureFunctionAttribute).Assembly)
+        .WithReference(typeof(FunctionContext).Assembly)
+        .WithReference(typeof(HttpRequest).Assembly)
+        .WithReference(typeof(IActionResult).Assembly)
+        .WithReference(typeof(ILogger<>).Assembly)
+        .WithReference(typeof(IServiceCollection).Assembly);
+
+    public static GeneratorResult RunGenerator(string source)
+    {
+        var result = Runner.Run(source);
+
+        return new GeneratorResult(
+            [.. result.GeneratorDiagnostics],
+            result.GeneratedSources,
+            result.AllGeneratedText);
+    }
 
     public static void AssertNoGeneratorErrors(GeneratorResult result)
     {
         var errors = result.Diagnostics
-            .Where(static d => d.Severity == DiagnosticSeverity.Error)
+            .Where(static x => x.Severity == DiagnosticSeverity.Error)
             .ToArray();
 
-        Assert.True(errors.Length == 0, string.Join(Environment.NewLine, errors.Select(static d => d.ToString())));
-    }
-
-    public static GeneratorResult RunGenerator(string source)
-    {
-        var parseOptions = new CSharpParseOptions(LanguageVersion.Preview);
-        var syntaxTree = CSharpSyntaxTree.ParseText(source, parseOptions);
-
-        var globalUsings = CSharpSyntaxTree.ParseText(GlobalUsings, parseOptions);
-        var compilation = CSharpCompilation.Create(
-            "TestAssembly",
-            [syntaxTree, globalUsings],
-            GetMetadataReferences(),
-            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-        GeneratorDriver driver = CSharpGeneratorDriver.Create(
-            [new FunctionGenerator().AsSourceGenerator()],
-            parseOptions: parseOptions);
-
-        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generatorDiagnostics);
-        var runResult = driver.GetRunResult();
-        var diagnostics = outputCompilation.GetDiagnostics()
-            .Concat(generatorDiagnostics)
-            .Concat(runResult.Diagnostics)
-            .Distinct()
-            .ToImmutableArray();
-        var sources = runResult.Results
-            .SelectMany(static r => r.GeneratedSources)
-            .ToDictionary(static s => s.HintName, static s => s.SourceText.ToString());
-        var generatedCode = string.Join(
-            Environment.NewLine + Environment.NewLine,
-            runResult.Results
-                .SelectMany(static r => r.GeneratedSources)
-                .Select(static s => s.SourceText.ToString()));
-
-        return new GeneratorResult(diagnostics, sources, generatedCode);
-    }
-
-    private static ImmutableArray<MetadataReference> GetMetadataReferences()
-    {
-        var trustedAssemblies = ((string?)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES"))?
-            .Split(Path.PathSeparator)
-            ?? [];
-        var assemblyPaths = new HashSet<string>(trustedAssemblies, StringComparer.OrdinalIgnoreCase)
-        {
-            typeof(IServiceCollection).Assembly.Location,
-            typeof(ILogger<>).Assembly.Location,
-            typeof(AzureFunctionAttribute).Assembly.Location,
-            typeof(FunctionGenerator).Assembly.Location,
-            typeof(FunctionContext).Assembly.Location,
-            typeof(HttpRequest).Assembly.Location,
-            typeof(IActionResult).Assembly.Location
-        };
-
-        return [.. assemblyPaths.Select(static path => (MetadataReference)MetadataReference.CreateFromFile(path))];
+        Assert.True(errors.Length == 0, String.Join(Environment.NewLine, errors.Select(static x => x.ToString())));
     }
 
     public sealed record GeneratorResult(
