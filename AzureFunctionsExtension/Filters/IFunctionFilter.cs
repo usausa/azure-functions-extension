cf799764e2aff241850929f25df41b760405b01b
usausa <machi.pon@gmail.com>
2026-09-01 17:16:14 +0900

    Update

--- AzureFunctionsExtension/Filters/IFunctionFilter.cs
+++ AzureFunctionsExtension/Filters/IFunctionFilter.cs
@@ -4,5 +4,7 @@ using System.Threading.Tasks;
 
 public interface IFunctionFilter
 {
+#pragma warning disable CA17116
     ValueTask InvokeAsync(FunctionInvocationContext context, FunctionFilterDelegate next);
+#pragma warning restore CA17116
 }
